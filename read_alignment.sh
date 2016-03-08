e30c55911475cb60fd193d5df0e6efd7eef8a2af
Pawel <pawel.sggw@gmail.com>
2016-03-08 14:42:02 +0100

    new files

--- read_alignment.sh
+++ read_alignment.sh
@@ -19,55 +19,59 @@
 
 mkdir ./"$2"
 
-#READ PREPARATION
+echo 'READ PREPARATION'
 #read adapter trimming, quality trimming - choose proper adapter fasta file in ./soft/Trimmomatic/adapters:
 #trimmomatic: http://www.usadellab.org/cms/index.php?page=trimmomatic
-echo 'Trimmomatic'
-echo ''
-java -jar ./soft/Trimmomatic-0.35/trimmomatic-0.35.jar PE ./"$2"_fw.fq ./"$2"_rv.fq ./"$2"/tt_"$2"_fw.fq ./"$2"/tt_"$2"_fw_unpaired.fq ./"$2"/tt_"$2"_rv.fq ./"$2"/tt_"$2"_rv_unpaired.fq ILLUMINACLIP:./soft/Trimmomatic-0.35/adapters/TruSeq3-PE.fa:2:30:15 TRAILING:30 MINLEN:50
+echo 'Trimmomatic' > echo ''
+#java -jar ./soft/Trimmomatic-0.35/trimmomatic-0.35.jar PE ./"$2"_fw.fq ./"$2"_rv.fq ./"$2"/tt_"$2"_fw.fq ./"$2"/tt_"$2"_fw_unpaired.fq ./"$2"/tt_"$2"_rv.fq ./"$2"/tt_"$2"_rv_unpaired.fq ILLUMINACLIP:./soft/Trimmomatic-0.35/adapters/TruSeq3-PE.fa:2:30:15 TRAILING:30 MINLEN:50
 echo''
 
 #read normalization, kmer error correction and kmer distribution histograms
 #bbmap
-echo 'bbmap'
-echo ''
-./soft/bbmap/ecc.sh in=./"$2"/tt_"$2"_fw.fq in2=./"$2"/tt_"$2"_rv.fq out1=./"$2"/ecc_"$2"_fw.fq out2=./"$2"/ecc_"$2"_rv.fq hist=./"$2"/bbmap_kmer_"$2"_in_hist histout=./"$2"/bbmap_kmer_"$2"_out_hist
+echo 'bbmap' > echo ''
+#./soft/bbmap/ecc.sh in=./"$2"/tt_"$2"_fw.fq in2=./"$2"/tt_"$2"_rv.fq out1=./"$2"/ecc_"$2"_fw.fq out2=./"$2"/ecc_"$2"_rv.fq hist=./"$2"/bbmap_kmer_"$2"_in_hist histout=./"$2"/bbmap_kmer_"$2"_out_hist
 #./soft/bbmap/bbnorm.sh in=./"$2"/tt_"$2"_fw_unpaired.fq  out=./"$2"/ecc_"$2"_fw_unpaired.fq target=40 mindepth=3 ecc cec=t hist=./"$2"/kmer_"$2"_up_fw_in_hist histout=./"$2"/kmer_"$2"_up_fw_out_hist
 #./soft/bbmap/bbnorm.sh in=./"$1"/tt_"$2"_rv_unpaired.fq  out=./"$2"/ecc_"$2"_rv_unpaired.fq target=40 mindepth=3 ecc cec=t hist=./"$2"/kmer_"$1"_up_rv_in_hist histout=./"$2"/kmer_"$2"_up_rv_out_hist
 echo ''
 
-# 1. ALIGNMENT with MOSAIK
-echo 'MOSAIK'
-echo ''
+echo '1. ALIGNMENT with MOSAIK' > echo 'MOSAIK' > echo ''
 #reference conversion
-./soft/MOSAIK/build/MosaikBuild -fr ./"$1" -oa mosaik_"$1".dat
+#./soft/mosaik_2.2.3/MosaikBuild -fr ./"$1" -oa mosaik_"$1".dat
 #read conversion
-./soft/MOSAIK/build/MosaikBuild -quiet -mfl 300 -st illumina -q ./"$2"/ecc_"$2"_fw.fq -q2 ./"$2"/ecc_"$2"_rv.fq -out ./"$2"/"$2".dat
+#./soft/mosaik_2.2.3/MosaikBuild -quiet -st illumina -q ./"$2"/ecc_"$2"_fw.fq -q2 ./"$2"/ecc_"$2"_rv.fq -out ./"$2"/"$2".dat
 #build data
-./soft/MOSAIK/build/MosaikJump -ia mosaik_"$1".dat -out mosaik_"$1" -hs 15 -mem 250
+#./soft/mosaik_2.2.3/MosaikJump -ia mosaik_"$1".dat -out mosaik_"$1" -hs 15 -mem 250
 #alignment
-./soft/MOSAIK/build/MosaikAligner -p 60 -quiet -annse ./soft/mosaik_2.2.3/networkFile/2.1.26.se.100.005.ann -annpe ./soft/mosaik_2.2.3/networkFile/2.1.26.pe.100.0065.ann -ia mosaik_"$1".dat -in ./"$2"/"$2".dat -out ./"$2"/"$2"
+#./soft/mosaik_2.2.3/MosaikAligner -p 60 -quiet -annse ./soft/mosaik_2.2.3/networkFile/2.1.26.se.100.005.ann -annpe ./soft/mosaik_2.2.3/networkFile/2.1.26.pe.100.0065.ann -ia mosaik_"$1".dat -in ./"$2"/"$2".dat -out ./"$2"/"$2"
 #export and duplicate removal
-#./soft/MOSAIK/build/MosaikText -in ./"$2"/"$2".bam -bam ./"$2"/"$2"_unique.bam
+#./soft/mosaik_2.2.3/MosaikText -in ./"$2"/"$2".bam -bam ./"$2"/"$2"_unique.bam
 echo ''
+#converting bam to sam
+#./soft/samtools-1.3/samtools view -h -o ./"$2"/"$2".sam ./"$2"/"$2".bam
 
 #additional ALIGNMENTS
 #INDEXING genome for alignment with bwa
+echo 'BWA: indexing genome'
 #./soft/bwa-0.7.7/bwa index "$1"
+
+echo ''
 # ALIGNMENT with bwa
-#./soft/bwa-0.7.12/bwa mem -t 10 "$1" "$2"_fw.fq "$2"_rv.fq > ./"$2"/"$2".sam
+echo 'BWA MEM' > echo ''
+./soft/bwa-0.7.12/bwa mem -t 20 -R '@RG\tID:foo\tSM:bar' "$1" ./"$2"/ecc_"$2"_fw.fq ./"$2"/ecc_"$2"_rv.fq > ./"$2"/"$2".sam
 #secondary ALIGNMENT with bbmap (more sensitive but some errors in .bam files)
 #./soft/bbmap/bbmap.sh ref="$1" in="$2"_fw.fq in2="$2"_rv.fq out=./"$2"/"$2".sam
 
-#converting bam to sam
-./soft/samtools-1.3/samtools view -h -o ./"$2"/"$2".sam ./"$2"/"$2".bam
 
-# 2. DUPLICATE REMOVAL with samblaster version 0.1.22
-./soft/samblaster/samblaster -i ./"$2"/"$2".sam -o ./"$2"/"$2"_marked.sam
+echo '2. DUPLICATE REMOVAL' > echo '' > echo 'SAMBLASTER'
+#./soft/samblaster/samblaster -i ./"$2"/"$2".sam -o ./"$2"/"$2"_marked.sam
 
-# 3. CONVERTING TO BAM with samtools 1.2 using htslib 1.2.1
 
-./soft/samtools-1.3/samtools view -Sb -o ./"$2"/"$2".bam ./"$2"/"$2"_marked.sam
-./soft/samtools-1.3/samtools sort -o ./"$2"/"$2"_sorted.bam -@ 20 ./"$2"/"$2"_uniwue.bam
+echo '3. CONVERTING TO BAM' #htslib 1.2.1
+echo '' > echo 'samtools'
+#./soft/samtools-1.3/samtools view -Sb -o ./"$2"/"$2".bam ./"$2"/"$2"_marked.sam
+
+# ADDING READ GROUPS
+./soft/bamaddrg/bamaddrg -b ./"$2"/"$2".bam -s B10 -r group_"$2"
+./soft/samtools-1.3/samtools sort -o ./"$2"/"$2"_sorted.bam -@ 20 ./"$2"/"$2".bam
 ./soft/samtools-1.3/samtools index ./"$2"/"$2"_sorted.bam
 
--- variants_call.sh
+++ variants_call.sh
@@ -1,63 +1,52 @@
-#Pawel Osipowski 16.11.2016
-#SCRIPT for read alignment and variant calling
-#read files should be deduplicated and trimmed of adapter 
-#sequences. trimmomatic can do this for instance.
+#Pawel Osipowski 17.02.2016
+#SCRIPT for variant calling from more than one .bam file
 
-#SOFTWARE:
-#ALIGNMENT: bbmap(more sensitive) or bwa
+
+#STEPS:
 #PROCESSING after: samblaster, samtools
-#VARIANT_CALLASSEMBLY with SOAPdenovo2, version 2.04-r240
 
 
 # SYNOPIS:
 # sh variant_call.sh <genome_name.fasta> reads_name
 
-# all paired read files must be in a form: <read_name>_fw.fq <read_name>_rv.fq
-
-#When using bwa for alignment separate genome
-#indexing is needed (hashed command in the script below)
-
-mkdir ./"$2"
-
-#INDEXING genome for alignment with bwa
-
-./soft/bwa-0.7.7/bwa index "$1"
-# 1. ALIGNMENT with bwa
-~/biosoft/read_alignment/bwa-0.7.7/bwa mem -t 10 "$1" "$2"_fw.fq "$2"_rv.fq > ./"$2"/"$2".sam
-
-# 1. ALIGNMENT with bbmap (more sensitive but .bam generates some errors along this pipeline)
-#./soft/bbmap/bbmap.sh ref="$1" in="$2"_fw.fq in2="$2"_rv.fq out=./"$2"/"$2".sam
-
-# 2. DUPLICATE REMOVAL with samblaster version 0.1.22
-./soft/samblaster/samblaster -i ./"$2"/"$2".sam -o ./"$2"/"$2"_marked.sam 
-
-# 3. CONVERTING TO BAM with samtools 1.2 using htslib 1.2.1
-./soft/samtools-1.2/samtools view -Sb -o ./"$2"/"$2".bam ./"$2"/"$2"_marked.sam
-./soft/samtools-1.2/samtools sort ./"$2"/"$2".bam ./"$2"/"$2"_sorted
-./soft/samtools-1.2/samtools index ./"$2"/"$2"_sorted.bam
-
-# 4. LCR (low complexity regions) specification on genome with mdust
-./soft/mdust/mdust "$1" -w 6 -v 20 -c > ./"$2"/"$2"_lcr.bed
+# LCR (low complexity regions) specification on genome with mdust
+#./soft/mdust/mdust "$1" -w 6 -v 20 -c > ./"$2"/"$2"_lcr.bed
 
 # 5. VARIANT CALLING with freebayes v0.9.21-19-gc003c1e
 # allele balance filter - 50% to call
-./soft/freebayes/bin/freebayes -p 1 --min-alternate-fraction 0.5 -F 0.4 -f "$1" ./"$2"/"$2"_sorted.bam > ./"$2"/"$2".vcf
+#./soft/freebayes/bin/freebayes -f "$1" -L ./"$2"/"$2"_bam.list > ./"$2"/"$2".vcf
+
+./soft/tabix-0.2.6/bgzip ./"$2"/"$2".vcf
+./soft/tabix-0.2.6/tabix ./"$2"/"$2".vcf.gz
+./soft/bcftools-1.3/bcftools stats ./"$2"/"$2".vcf.gz > ./"$2"/1_"$2".stats
+./soft/tabix-0.2.6/bgzip -d ./"$2"/"$2".vcf.gz
 
 # 6. VARIANT FILTERING vcflib
 #variant quality filtering with vcffilter
 ./soft/vcflib/bin/vcffilter -f "QUAL > 30" ./"$2"/"$2".vcf > ./"$2"/"$2"_q30.vcf
 
+./soft/tabix-0.2.6/bgzip ./"$2"/"$2"_q30.vcf
+./soft/tabix-0.2.6/tabix ./"$2"/"$2"_q30.vcf.gz
+./soft/bcftools-1.3/bcftools stats ./"$2"/"$2"_q30.vcf.gz > ./"$2"/2_"$2"_q30.stats
+./soft/tabix-0.2.6/bgzip -d ./"$2"/"$2"_q30.vcf.gz
+
 # lcr read filtering with lcr_filter.py
-python ./soft/lcr_filter.py ./"$2"/"$2"_lcr.bed ./"$2"/"$2"_q30.vcf ./"$2"/"$2"_lcrfree.vcf ./"$2"/"$2"_inlcr_var.txt
+python ./soft/lcr_filter.py ./"$1"_lcr.bed ./"$2"/"$2"_q30.vcf ./"$2"/"$2"_LCRfree.vcf ./"$2"/"$2"_inlcr_var.txt
+
+./soft/tabix-0.2.6/bgzip ./"$2"/"$2"_LCRfree.vcf
+./soft/tabix-0.2.6/tabix ./"$2"/"$2"_LCRfree.vcf.gz
+./soft/bcftools-1.3/bcftools stats ./"$2"/"$2"_LCRfree.vcf.gz > ./"$2"/3_"$2"_LCRfree.stats
+./soft/tabix-0.2.6/bgzip -d ./"$2"/"$2"_LCRfree.vcf.gz
 
 #variant read depth filtering(MD); if either the number of non-reference reads on the forward strand or on the reverse strand is
 #below treshold (SAF & SAR)
 
 #count depth score for max MD filtering (Li H, 2014)
 #samtools (this is valid - takes only covered bases to count average)
-avg_depth=$(./soft/samtools-1.3/samtools depth ./0/0_sorted.bam  |  awk '{sum+=$3} END { print sum/NR}'| bc -l)
-depth_score=$(echo "$avg_depth + sqrt($avg_depth) * 3" | bc -l)
-echo 'average loci base coverage is: ' $avg_depth ', and depth score is: ' $depth_score
+#avg_depth=$(./soft/samtools-1.3/samtools depth ./bam/"$2"_sorted.bam  |  awk '{sum+=$3} END { print sum/NR}'| bc -l)
+#depth_score=$(echo "$avg_depth + sqrt($avg_depth) * 3" | bc -l)
+#echo 'average loci base coverage is: ' $avg_depth ', and depth score is: ' $depth_score
+
 #bedtools (this is not valid - 0 coverage bases are also taken to count average depth)
 #keeping it for future
 #./soft/bedtools2/bin/bedtools genomecov -it -ibam ./"$2"/"$2"_sorted.bam > ./"$2"/"$2"_coverage_hist.txt
@@ -65,17 +54,20 @@ echo 'average loci base coverage is: ' $avg_depth ', and depth score is: ' $dept
 #depth_score=$(echo "$avg_depth + sqrt($avg_depth) * 3" | bc -l)
 #echo 'average loci base coverage is: ' $avg_depth ', and depth score is: ' $depth_score
 
-#max MD (treshold=depth score from Li H., 2014)
-./soft/vcflib/bin/vcffilter -f "DP < $depth_score" ./"$2"/"$2"_lcrfree.vcf > ./"$2"/"$2"_DP.vcf
 #min MD (treshold=10)
-./soft/vcflib/bin/vcffilter -f "DP > 10" ./"$2"/"$2"_DP.vcf > ./"$2"/"$2"_DP2.vcf
+./soft/vcflib/bin/vcffilter -f "DP > 10" ./"$2"/"$2"_LCRfree.vcf > ./"$2"/"$2"_minDP.vcf
+
+./soft/tabix-0.2.6/bgzip ./"$2"/"$2"_minDP.vcf
+./soft/tabix-0.2.6/tabix ./"$2"/"$2"_minDP.vcf.gz
+./soft/bcftools-1.3/bcftools stats ./"$2"/"$2"_minDP.vcf.gz > ./"$2"/4_"$2"_minDP.stats
+./soft/tabix-0.2.6/bgzip -d ./"$2"/"$2"_minDP.vcf.gz
+
 #SAF&SAR (treshold=5)
-./soft/vcflib/bin/vcffilter -f "SAF > 5" ./"$2"/"$2"_DP2.vcf > ./"$2"/"$2"_SAF.vcf
-./soft/vcflib/bin/vcffilter -f "SAR > 5" ./"$2"/"$2"_SAF.vcf > ./"$2"/"$2"_filt.vcf
+./soft/vcflib/bin/vcffilter -f "SAF > 5" ./"$2"/"$2"_minDP.vcf > ./"$2"/"$2"_SAF.vcf
+./soft/vcflib/bin/vcffilter -f "SAR > 5" ./"$2"/"$2"_SAF.vcf > ./"$2"/"$2"_SA.vcf
 
 # 7. POST DATA preparation with bcftools
-bgzip ./"$2"/"$2"_filt.vcf
-tabix ./"$2"/"$2"_filt.vcf.gz
-./soft/bcftools-1.2/bcftools stats ./"$2"/"$2"_filt.vcf.gz > ./"$2"/"$2".stats
-
-
+./soft/tabix-0.2.6/bgzip ./"$2"/"$2"_SA.vcf
+./soft/tabix-0.2.6/tabix ./"$2"/"$2"_SA.vcf.gz
+./soft/bcftools-1.3/bcftools stats ./"$2"/"$2"_SA.vcf.gz > ./"$2"/5_"$2"_SA.stats
+./soft/tabix-0.2.6/bgzip -d ./"$2"/"$2"_SA.vcf.gz
